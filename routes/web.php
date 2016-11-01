1570719a8899b2a527d56817fecdf5e29971e092
migoran78 <mi.goran@hotmail.com>
2016-11-02 00:40:20 +0100

    h1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Post;
+use App\User;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +16,44 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/create', function (){
+    $user = User::findOrFail(1);
+    $post = new Post(['title'=>'My first post 2', 'body'=>'Laravel body 2']);
+    $user->posts()->save($post);
+
+});
+
+
+
+Route::get('/read', function (){
+    $user = User::findOrFail(1);
+//    dd($user->posts);
+//    dd($user);
+
+    foreach ($user->posts as $post){
+        echo $post->title . "<br>";
+    }
+});
+
+
+
+Route::get('/update', function(){
+    $user = User::find(1);
+
+//    $user->posts()->where('id','=','2')->update(['title'=>'title - updated field 2', 'body'=>'body - updated field 2']);
+    $user->posts()->whereId(1)->update(['title'=>'title - updated field', 'body'=>'body - updated field']);
+
+});
+
+
+
+Route::get('/delete', function (){
+    $user = User::find(1);
+
+    $user->posts()->whereId(2)->delete();
+//    $user->posts()->delete();
 });
